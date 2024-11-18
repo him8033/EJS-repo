d39274ff0b238110c25c1f8d63a6f3147b52aa2b
him8033 <himanshu8033@gmail.com>
2024-11-18 20:21:02 +0530

    for loop syntax implement in ejs

--- index.js
+++ index.js
@@ -21,11 +21,19 @@ app.get("/home",(req,res) => {
     console.log("requesting...")
 })
 
-// app.get("/:username",(req,res) => {
-//     let {username} = req.params;
-//     res.render("instagram.ejs",{username});
-//     console.log("requesting...");
-// })
+app.get("/:username",(req,res) => {
+    let {username} = req.params;
+    res.render("instagram.ejs",{username});
+    console.log("requesting...");
+})
+
+app.get("/ig/:username",(req,res) => {
+    const followers = ["adam","bob","steve","john"];
+    let {username} = req.params;
+    res.render("forloop.ejs",{username,followers});
+    console.log("requesting...");
+})
+
 app.get("/rolldice",(req,res) => {
     let diceValue = Math.floor(Math.random()*6)+1;
     res.render("rollDice.ejs",{diceValue});
